f60d3157893ca2eb4f3845b56b5ec782680aefb6
mazin.sidahmed <mazin.sidahmed@guardian.co.uk>
2017-11-12 18:34:27 -0500

    Added ContactList and ContactCard

--- app/src/index.js
+++ app/src/index.js
@@ -13,9 +13,61 @@ class ReportersNotebook extends React.Component {
 		return (
 			<div>
 				<AppBar />
+				<ContactList />
 			</div>
 		)
 	}
+}
+
+class ContactCard extends React. Component {
+      render() {
+      return(
+        <div className="contact-card">
+        <div> {this.props.name} </div>
+        <div> {this.props.information} </div>
+        </div>
+      )
+      }
+
+}
+
+class ContactList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      contacts: [
+        {
+          name: "Connor Jennings",
+          information: "123-456-789"
+        },
+        {
+          name: "Mazin Sidahmed",
+          information: "XXX-XXX-XXX"
+        }
+      ]
+
+    }
+  }
+  render() {
+    let contactCards = [];
+    for(let i=0; i<this.state.contacts.length; i++) {
+      let contactCard = (
+        <ContactCard
+          key={this.state.contacts[i].name}
+          name={this.state.contacts[i].name}
+          information={this.state.contacts[i].information}
+          >
+        </ContactCard>
+      )
+      contactCards.push(contactCard)
+    }
+
+    return(
+    <div className="contact-list">
+      {contactCards}
+    </div>
+  )
+  }
 }
 
 ReactDOM.render(
